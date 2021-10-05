489570c157d84c432210cdd5fb4db4ae2ce98832
Marcus Rothoff <marcusrothoff@hotmail.com>
2021-10-05 19:43:10 +0200

    formatting and create array in js

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let cardArray = [
+        { name: "athens", img: "assets/images/athens.png" },
+        { name: "athens", img: "assets/images/athens.png" },
+        { name: "barcelona", img: "assets/images/barcelona.png" },
+        { name: "barcelona", img: "assets/images/barcelona.png" },
+        { name: "bari", img: "assets/images/bari.png" },
+        { name: "bari", img: "assets/images/bari.png" },
+        { name: "porto", img: "assets/images/porto.png" },
+        { name: "porto", img: "assets/images/porto.png" },
+        { name: "pisa", img: "assets/images/pisa.png" },
+        { name: "pisa", img: "assets/images/pisa.png" },
+        { name: "rome", img: "assets/images/rome.png" },
+        { name: "rome", img: "assets/images/rome.png" },
+    ];
+});
